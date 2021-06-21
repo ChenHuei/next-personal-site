a411afe2396f92e1aaf2eec27e779218ee2a703c
leo <leo.chen@cloud-interactive.com>
2021-06-21 17:35:10 +0800

    Add fontawesome and normalize plugin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,9 @@
 import type { AppProps } from 'next/app'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import 'normalize.css'
+import '@/styles/globals.css'
+
+config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />

9952d37b1dfadf1d9b1f1654faa9cd615ae6c7a0
LeoChen <leo_chen@mayohr.com>
2024-04-18 10:51:08 +0800

    add next-optimized-images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const withOptimizedImages = require('next-optimized-images')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export'
 }
 
-module.exports = nextConfig
+module.exports = withOptimizedImages(nextConfig)
